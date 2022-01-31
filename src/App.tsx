03dfdf0c420f0440fcb82c4774ee0ce5b7479dee
Tri Truong <truongductri01@gmail.com>
2022-01-30 21:08:01 -0500

    handle rotate event

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,9 @@ function App() {
     window.addEventListener("resize", () => {
       setAppHeight(window.innerHeight);
     });
+    window.addEventListener("orientationchange", () => {
+      setAppHeight(window.innerHeight);
+    });
   }, []);
 
   useEffect(() => {
